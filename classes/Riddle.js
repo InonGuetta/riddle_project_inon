91f786f09ebe57dcb7f2e011e7bab5ed16130026
Inon Guetta <inon16guetta@gmail.com>
2025-06-29 10:34:17 +0300

    core_program

--- classes/Riddle.js
+++ classes/Riddle.js
@@ -1,22 +1,38 @@
+// בזכות הספרייה הזאת ניתן להשתמש בהזנת ערך הפונקצייה
+import readlineSync from 'readline-sync';
+
 import {allQ} from './../main_manger/mangerRiddle.js'
 
 
 
 class Riddle{
-    constructor(){
-
+    constructor(id,name,taskDescription,correctAnswer){
+        this.id = id;
+        this.name = name;
+        this.taskDescription = taskDescription;
+        this.correctAnswer = correctAnswer;
     }
-
-
+    
     ask(){
-        // הדפסה של השאלה 
-        // קבלת התשובה מהמשתמש 
-        // בדיקה האם התשובה נכונה או לא 
-
-
-        // תחשוב איך אתה עובר לשאלה הבאה 
-
+        console.log(this.taskDescription);
+        let pause = false;
+        while(!pause){
+            let user_answer = readlineSync.question('please give here you answer :');
+            if (user_answer == this.correctAnswer){
+                console.log("Your answer is correct \n");        
+                pause = true;
+            }else{
+                console.log("you worng please try again: \n");        
+                console.log(this.taskDescription); 
+            }
+        }
     }
 }
+
+for(let i = 0;i<allQ.length;i++){
+    const to_riddle = new Riddle(allQ[i].id,allQ[i].name,allQ[i].taskDescription,allQ[i].correctAnswer);     
+    to_riddle.ask();
+}
+
 
 

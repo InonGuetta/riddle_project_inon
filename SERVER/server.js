3b5e9be79d0580ac615b5d57121cb65313ce3418
Inon Guetta <inon16guetta@gmail.com>
2025-07-25 16:41:26 +0300

    combain_mongo_seq

--- SERVER/server.js
+++ SERVER/server.js
@@ -1,37 +1,20 @@
-// import express from 'express';
-// // הport שאני צריך על מנת להתחבר לשרת
-// const PORT = process.env.PORT || 3000;
-// // ייבוא הקוד של toPlayrs.js מהראוט 
-// // כדי שנוכל להשתמש בנתיב הנכון של השליחה לשרת
-// import toPlayers from "./routes/toPlayers.js";
-// // הקובץ שמכיל את הקונפוגרציה הנכונה של איך לשלוח לשרת 
-// import sequelize from './config/db.config.js';
-
-// // הפעלת השרת ע"י שימוש במתודה express 
-// const server = express();
-
-// // יצירת המידל וואר עבור שיוכל להשתמש בקבלת קבצי json כלומר בקבלת אובייקטים  
-// server.use(express.json());
-// // הוא משתמש עבור יצירת המיקום של ה db והתוצרה הנכונה שלו 
-// await sequelize.sync();
-
-// toPlayers(server);
-
-// server.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
-
 import express from "express";
 import dotenv from "dotenv";
 import configRoutes from "./routes/configRoutes.js";
+import sequelize from './config/db.config.js';
+import toPlayers from "./routes/toPlayers.js";
+
 
 dotenv.config();
 
-const app = express();
-app.use(express.json());
-configRoutes(app);
+const server = express();
+server.use(express.json());
+configRoutes(server);
+await sequelize.sync();
+toPlayers(server);
+
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log("Server running on port", PORT);
 });
